32a3c73b951b0840157effe9e740caae4083050f
djawed-bkh <bekhouchadjawed@gmail.com>
2017-12-16 19:41:59 +0100

    16/12/17

--- src/sample/compte.java
+++ src/sample/compte.java
@@ -62,6 +62,9 @@ public class compte {
     private int dollar = 3000;
     private int inter;
     private String name = "inconnu";
+    private boolean var=true;
+    private int clav=0;
+    private int op;
     //initialisations
 
     public void menu(ActionEvent event) {
@@ -70,29 +73,27 @@ public class compte {
             screen2.setText("details  press 1 on the screen \n verser dollar press 2 on the screen\n verser euro press 3 on the screen\n retrait dollar press 4 on the screen\n retrait euro press 5 on the screen");
         } else if (event.getSource() == e2) {
             screen1.setText("VERSEMENT DOLLAR");
-            screen2.setText("tappez la somme a verser");
+            screen2.setText("");
             sasie(event);
-            dollar += inter;
-            inter=0;
+          op=2;
         }
         else if (event.getSource() == e3) {
             screen1.setText("VERSEMENT EURO");
+            var=false;
             sasie(event);
-            euro+=inter;
-            inter=0;
-
+           op=3;
         } else if (event.getSource() == e4) {
             screen1.setText("RETRAIT DOLLAR");
-            screen2.setText("tappez la somme a retirer");
+            screen2.setText("");
+            var=false;
             sasie(event);
-           dollar-=inter;
-            inter=0;
+           op=4;
         } else if (event.getSource() == e5) {
             screen1.setText("RETRAIT EURO");
-            screen2.setText("tappez la somme a retirer");
+            screen2.setText("");
+            var=false;
             sasie(event);
-            euro-=euro;
-            inter=0;
+            op=5;
         }
 
         else if (event.getSource() == e1) {
@@ -100,41 +101,66 @@ public class compte {
             screen1.setText(" DÉTAILS COMPTE");
             screen2.setText("NOM : " + name + "\n" + screen2.getText() + "EURO :" + euro + "\n" + screen2.getText() + "dollar :" + dollar);
         }
+        if (op==2){
+            dollar+=inter;
+            inter=0;
+        }else if (op==3) {
+            euro+=inter;
+            inter=0;
+        }else if (op==4){
+            dollar-=inter;
+            inter=0;
+
+        }else if (op==5) {
+            euro -= inter;
+            inter=0;
+        }
 
     }
     public void sasie(ActionEvent event){
-        if (event.getSource() == n1) {
+        if (var==false &&clav==0){
+            screen2.setText("");}
+       if (event.getSource() == n1) {
+           clav++;
             screen2.setText(screen2.getText() + "1");
         } else if ((event.getSource() == n2)) {
+           clav++;
             screen2.setText(screen2.getText() + "2");
         } else if ((event.getSource() == n3)) {
+           clav++;
             screen2.setText(screen2.getText() + "3");
         } else if ((event.getSource() == n4)) {
+           clav++;
             System.out.println("n1" + event.getSource());
 
             screen2.setText(screen2.getText() + "4");
         } else if ((event.getSource() == n5)) {
-
+           clav++;
             screen2.setText(screen2.getText() + "5");
         } else if ((event.getSource() == n6)) {
-
+           clav++;
             screen2.setText(screen2.getText() + "6");
         } else if ((event.getSource() == n7)) {
-
+           clav++;
             screen2.setText(screen2.getText() + "7");
         } else if ((event.getSource() == n8)) {
-
+           clav++;
             screen2.setText(screen2.getText() + "8");
         } else if ((event.getSource() == n9)) {
-
+           clav++;
             screen2.setText(screen2.getText() + "9");
         } else if ((event.getSource() == n0)) {
-            System.out.println("n1" + event.getSource());
+           clav++;
             screen2.setText(screen2.getText() + "0");
         } else if (event.getSource() == entrer) {
-            inter = Integer.parseInt(screen2.getText());
+           clav=0;
+           inter = Integer.parseInt(screen2.getText());
+            screen2.setText("Operation effectué correctement");
+
+
 
         }
+
     }
 }
 
